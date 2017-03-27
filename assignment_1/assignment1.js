5ce3a92b1aaf351ec5e1e41eadc4bcd990a79878
Sholan Narine <Sholan Narine>
2017-03-27 19:26:33 -0400

    json

--- assignment_1/assignment1.js
+++ assignment_1/assignment1.js
@@ -2,7 +2,7 @@
 Exercise #1
 Initialize an empty array below in the variable numbers.
 */
-var numbers = [0];
+var numbers = "";
 console.log('hello world')
 
 /*
@@ -10,7 +10,10 @@ Exercise #2
 Using the loop provided for you below, use an array's push method to insert the nummbers  0 to 10 into positions 0 through 10 of the numbers array you just initialized above.'If you're not sure how to use the array push method go back and re read this module on data strucutres.'
 */
 
-for(var i=0;i<10;i++){
+for(var i=0; i<= 10; i++)
+{
+for(var x=0; x<= i; i++)    
+console.log('0')
     //use the array push() method here.
 }
 
