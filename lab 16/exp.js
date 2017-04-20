7cd2984818aa0b5ba588ddcd869d31127266bb4f
Sholan Narine <Sholan Narine>
2017-04-20 13:27:47 -0400

    all my files update

--- lab 16/exp.js
+++ lab 16/exp.js
@@ -1,5 +1,8 @@
 var express = require ('express')
 var app = express ()
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded)
 
 app.get('/', function (req, res){
     res.send ('<h1>Hello World</h1>')
@@ -10,6 +13,13 @@ app.get('/goodbye', function (req, res){
     res.send ('<h1>Goodbye World</h1>')
 })
 
+
+app.post('/', function (req, res){
+    console.log(req.body);
+    res.send ('<h1>TEST</h1>')
+})
+
+
 app.listen (3000, function () {
-console.log('Example app listening on port 3000')
+console.log('Example app listening on port 3000');
 })
--- lab 16/lab.js
+++ lab 16/lab.js
@@ -1,16 +1,34 @@
 var express = require ('express')
 var app = express ()
 
+app.use(express.static('public'))
+
+
+app.get('/test', function (req, res){
+    res.send ('<p>Test Page</p>')
+})
+
 app.get('/name', function (req, res){
-    res.send ('Sholan')
+    res.send ('<p>Sholan</p>')
 })
 
 app.get('/sum', function (req, res){
-    res.send ('sum')
+
+    var a = parseInt(req.query.a);
+    var b = parseInt(req.query.b);
+    var c = parseInt(req.query.c);
+    sum = a + b + c;
+
+    res.send (sum.toString())
 })
 
 app.get('/color', function (req, res){
-    res.send ('color')
+    let color = {
+ red: 0,
+ green: 79,
+ blue: 255
+};
+    res.send (color)
 })
 
 app.listen (3000, function () {
